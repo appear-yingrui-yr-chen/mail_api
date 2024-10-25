3c3c003e5a92d50fcdd97d1ca3fda35e35a1f1d4
yingrui.yr.chen <yingrui.yr.chen@appier.com>
2024-10-25 10:20:18 +0800

    Update get token and operation.py

--- get token and operation.py
+++ get token and operation.py
@@ -9,7 +9,7 @@ response = requests.post(login_url, json=login_data)
 
 if response.status_code == 200:
     token = response.json()['token']
-    print(f"Login successful. Token: {token}")
+    #print(f"Login successful. Token: {token}")
 else:
     print(f"Failed to login: {response.status_code} - {response.text}") 
     
@@ -21,8 +21,21 @@ headers = {
 messages_url = "https://api.mail.tm/messages"
 response = requests.get(messages_url, headers=headers)
 if response.status_code == 200:
-    emails = response.json()
-    print(emails)
-    
+    emails = response.json()['hydra:member']   
+    for email in emails:
+        if email['from']['address'] =='airis@appier.com': #filter選取寄件者
+            mail_id=email['id']
+            realresponse=requests.get(messages_url+"/"+mail_id,headers=headers)
+            targetmail=realresponse.json()
+            print(targetmail['to'][0]['address']) #收件者
+            start=targetmail['intro'].find("join")+9
+            endpoint=targetmail['intro'].find("project")-1
+            print(f"project name: {targetmail['intro'][start:endpoint] }") ＃獲邀project
+            start2=targetmail['text'].find("link")+7
+            endpoint2=targetmail['text'].find("create")-5
+            print(targetmail['text'][start2:endpoint2]) #驗證連結
+           
+        
 else:
     print(f"Failed to retrieve emails: {response.status_code} - {response.text}")       
+    
